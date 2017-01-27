fc639e17de94b575d33275db10a027dad2ce94c9
Philippe Creux <pcreux@gmail.com>
2017-01-27 12:09:47 -0800

    db:seed creates users and cups

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+[
+  {
+    "slack_identifier"=>"123AB",
+    "name"=>"philippe",
+    "email"=>"philippe@example.com",
+    "first_name"=>"Philippe",
+    "last_name"=>"Creux",
+    "real_name"=>"Philippe Creux",
+    "avatar_72"=>"https://avatars.slack-edge.com/2016-10-11/90190409153_dd6490c09d607f12e4be_72.jpg",
+    "avatar_192"=>"https://avatars.slack-edge.com/2016-10-11/90190409153_dd6490c09d607f12e4be_192.jpg"
+  },
+  {
+    "slack_identifier"=>"234AB",
+    "name"=>"vitor",
+    "email"=>"vitor@example.com",
+    "first_name"=>"Vitor",
+    "last_name"=>"Gurgel",
+    "real_name"=>"Vitor Gurgel",
+    "avatar_72"=>"https://avatars.slack-edge.com/2016-10-05/87926542192_907e9f8d8502fbb137a9_72.jpg",
+    "avatar_192"=>"https://avatars.slack-edge.com/2016-10-05/87926542192_907e9f8d8502fbb137a9_192.jpg"
+  },
+  {
+    "slack_identifier"=>"22333ABC",
+    "name"=>"kyle",
+    "email"=>"kyle@example.com",
+    "first_name"=>"Kyle",
+    "last_name"=>"Kury",
+    "real_name"=>"Kyle Kury",
+    "avatar_72"=>"https://avatars.slack-edge.com/2016-07-18/60855270035_3c06e748fc91eb6f0739_72.jpg",
+    "avatar_192"=>"https://avatars.slack-edge.com/2016-07-18/60855270035_3c06e748fc91eb6f0739_192.jpg"
+  }
+].each do |attributes|
+  unless User.find_by_slack_identifier(attributes.fetch("slack_identifier"))
+    User.create!(attributes)
+  end
+end
+
+users = User.all.to_a
+
+100.times do
+  user = users.sample
+  cup = Cup.create!(
+    user_identifier: user.slack_identifier,
+    user_name: user.name
+  )
+
+  cup.update_attribute(:created_at, Time.now - rand(3600 * 24 * 60))
+end
