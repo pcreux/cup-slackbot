df3aaaaf182b1e2aaeb5f4be452361bf3e549ad1
Philippe Creux <pcreux@gmail.com>
2017-01-27 10:28:23 -0800

    Don't check CSRF token for slack commands

--- app/controllers/slack_controller.rb
+++ app/controllers/slack_controller.rb
@@ -1,4 +1,7 @@
 class SlackController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
   def create
     render plain: "Thanks for using a reusable cup! You rock!"
   end
